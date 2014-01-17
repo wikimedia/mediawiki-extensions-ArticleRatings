18479498e155d15c7d8005ab271c609dbb2fcbdc
UltrasonicNXT <adamr_carter@btinternet.com>
2014-01-17 19:30:26 +0000

    Initial code import

---
+++ ArticleRatings.i18n.php
@@ -0,0 +1,25 @@
+<?php
+
+$messages = array();
+/** English
+ * @author Adam Carter
+ */
+$messages['en'] = array(
+	'changerating' => 'ChangeRating',
+	'massratings' => 'MassRatings',
+	'log-name-ratings' => 'Rating Change Log',
+	'log-description-ratings' => 'This log shows all the recent changes to pages\' ratings.',
+	'logentry-ratings-change' => '$1 changed the rating of $3 to $4',
+	'ratings-desc' => 'A complex interface for rating pages'
+);
+/** Vietnamese/Việt
+ * @author Codyn329
+ */
+$message['vi'] = array(
+	'changerating' => 'Đánh giá sự thay đổi',
+	'massratings' => 'số đông đánh giá',
+	'log-name-ratings' => 'sự thay đổi Đánh giá ghi',
+	'log-descripiton-ratings' => 'Nhật ký này cho thấy tất cả các thay đổi gần đây để các trang\'s Dánh giá.',
+	'logentry-ratings-change' => '$1 thay đổi đánh giá của $3 đến $4',
+	'rating-desc' =>'Một giao diện phức tạp để đánh giá các trang'
+);
