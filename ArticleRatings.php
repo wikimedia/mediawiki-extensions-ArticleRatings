00f70c9b7d821d09a3e63909cb76b3ea9c1708a7
UltrasonicNXT <adamr_carter@btinternet.com>
2014-01-23 19:48:53 +0000

    Bump version number

--- ArticleRatings.php
+++ ArticleRatings.php
@@ -66,7 +66,7 @@ $wgExtensionCredits['other'][] = array(
         'author' => 'UltrasonicNXT/Adam Carter',
         'url' => 'https://www.mediawiki.org/wiki/Extension:ArticleRatings',
         'descriptionmsg' => 'ratings-desc',
-        'version' => '1.3',
+        'version' => '2.0',
 );
 
 # Special:SpecialPages
