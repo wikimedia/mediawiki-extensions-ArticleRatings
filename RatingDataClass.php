17e355e6f324e550e6ccf26d2e5f0d3c410b97b9
Jack Phoenix <jack@countervandalism.net>
2014-02-04 07:15:41 -0800

    In RatingDataClass.php: * coding style tweaks * removed some unused globals * swapped raw HTML to an Html::element() call in RatingData::getImage due to paranoia

--- RatingDataClass.php
+++ RatingDataClass.php
@@ -1,45 +1,43 @@
 <?php
 
-global $wgArticlePath;
-global $ratingsJSONName;
-
 class RatingData {
 
 	protected $thisCodename;
 	public $noImageString = '';
 	public $JSON = array();
 
-	public function __construct( $codename ){
+	public function __construct( $codename ) {
 		if ( empty( $codename ) ) {
 			trigger_error( 'Never seed RatingData with an empty codename' );
 		} else {
 			$this->thisCodename = $codename;
 		}
 		$json = wfMessage( 'are-ratings' )->plain();
-		if( empty( $json ) ) {
-			 trigger_error( 'ARE Error: empty JSON' );
+		if ( empty( $json ) ) {
+			trigger_error( 'ARE Error: empty JSON' );
 		}
 		$this->JSON = json_decode( $json, true );
 	}
 
 	public static function getAllRatings() {
 		$json = wfMessage( 'are-ratings' )->plain();
-		if( empty( $json ) ) {
+		if ( empty( $json ) ) {
 			trigger_error( 'ARE Error: empty JSON' );
 		}
 		$JSON = json_decode( $json, true );
 
 		$returners = array();
 
-		foreach( $JSON as $data ){
+		foreach ( $JSON as $data ) {
 			$returners[] = $data['codename'];
 		}
+
 		return $returners;
 	}
 
 	public function getAttr( $attr ) {
-		foreach( $this->JSON as $data ) {
-			if( $data['codename'] == $this->thisCodename ) {
+		foreach ( $this->JSON as $data ) {
+			if ( $data['codename'] == $this->thisCodename ) {
 				return $data[$attr];
 			}
 		}
@@ -50,11 +48,16 @@ class RatingData {
 		$data = $this->getAttr( 'img' );
 
 		$file = wfFindFile( $data );
-		if( !$file ) {
+		if ( !$file ) {
 			return $this->noImageString;
 		}
 		$image = $file->getCanonicalUrl();
-		$pic = '<img class="mw-rating-img" src="' . $image . '" height="20px" width="20px" /> ';
+		$pic = Html::element( 'img', array(
+			'class' => 'mw-rating-img',
+			'src' => $image,
+			'height' => '20px',
+			'width' => '20px'
+		) ) . wfMessage( 'word-separator' )->parse();
 
 		return $pic;
 	}
@@ -63,7 +66,7 @@ class RatingData {
 		$data = $this->getAttr( 'img' );
 
 		$file = wfFindFile( $data );
-		if( !$file ) {
+		if ( !$file ) {
 			return $this->noImageString;
 		}
 		return '[[File:' . $data . '|20px]] ';
@@ -71,10 +74,12 @@ class RatingData {
 
 	public function getAboutLink() {
 		global $wgArticlePath;
-		$link = $this->getAttr('link');
+
+		$link = $this->getAttr( 'link' );
 
 		$url = str_replace( '$1', $link, $wgArticlePath );
-  		$label = '<a class="mw-rating-about-link" href="' . $url . '" target="_blank">' . $this->getAttr( 'name' ) . '</a>';
+		$label = '<a class="mw-rating-about-link" href="' . $url . '" target="_blank">' .
+			$this->getAttr( 'name' ) . '</a>';
 
 		return $label;
 	}
