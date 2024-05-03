ac71a480e7542a718fb0479e8bf08ad971715daa
Jack Phoenix <ashley@uncyclomedia.co>
2024-05-03 14:11:48 +0300

    In preparation for creating an API (T146552), modularize and split up some of the code from SpecialChangeRating#execute
    The new API module will need to call these functions from its execute method.
    Doing this enables us to reduce unnecessary code duplication and will hopefully make the program workflow clearer for future people having to read this code.
    
    Change-Id: I388e33cc88f54a94ff95ef34eb50967efa77ba44

--- SpecialChangeRating.php
+++ SpecialChangeRating.php
@@ -47,8 +47,6 @@ class SpecialChangeRating extends SpecialPage {
 
 			$out->addWikiMsg( 'changerating-back', $title->getFullText() );
 
-			$dbr = wfGetDB( DB_REPLICA );
-
 			$ratingto = $request->getVal( 'ratingTo' );
 
 			$output = '';
@@ -60,47 +58,12 @@ class SpecialChangeRating extends SpecialPage {
 			) {
 				$ratingto = substr( $ratingto, 0, 2 );
 
-				$resOldRating = $dbr->selectField(
-					'ratings',
-					'ratings_rating',
-					[
-						'ratings_title' => $title->getDBkey(),
-						'ratings_namespace' => $title->getNamespace()
-					],
-					__METHOD__
-				);
+				// @todo FIXME: this is now _also_ done inside insertOrUpdateRating() :-(
+				// But we need the value here, too...
+				$resOldRating = self::getCurrentRatingForPage( $title );
 
-				$dbw = wfGetDB( DB_PRIMARY );
-				// If there is an entry, update it.
-				// If there isn't, we need to *create* it before we can even
-				// think of updating it :^)
-				// (Whaddya know, trying to UPDATE something that doesn't exist
-				// seems to fail silently, nothing tells you that "you gotta INSERT
-				// first", the code just fails and sends you on a wild goose chase
-				// for half an hour or so...)
-				if ( $resOldRating ) {
-					$res = $dbw->update(
-						'ratings',
-						[ 'ratings_rating' => $ratingto ],
-						[
-							'ratings_title' => $title->getDBkey(),
-							'ratings_namespace' => $title->getNamespace()
-						],
-						__METHOD__
-					);
-				} else {
-					$res = $dbw->insert(
-						'ratings',
-						[
-							'ratings_rating' => $ratingto,
-							'ratings_title' => $title->getDBkey(),
-							'ratings_namespace' => $title->getNamespace()
-						],
-						__METHOD__
-					);
-				}
+				self::insertOrUpdateRating( $ratingto, $title );
 
-				$reason = $request->getVal( 'reason' );
 				$out->addWikiMsg( 'changerating-success' );
 
 				$rating = new Rating( $ratingto );
@@ -112,19 +75,9 @@ class SpecialChangeRating extends SpecialPage {
 				}
 				$oldrating = new Rating( $seed );
 
-				$logEntry = new ManualLogEntry( 'ratings', 'change' );
-				$logEntry->setPerformer( $user );
-				$logEntry->setTarget( $title );
-				$logEntry->setParameters( [
-					'4::newrating' => $rating->getName(),
-					'5::oldrating' => $oldrating->getName()
-				] );
-				if ( $reason !== null ) {
-					$logEntry->setComment( $reason );
-				}
+				$reason = $request->getVal( 'reason' );
 
-				$logId = $logEntry->insert();
-				$logEntry->publish( $logId );
+				self::log( $user, $title, $rating, $oldrating, $reason );
 			} elseif ( $request->wasPosted() && !$user->matchEditToken( $request->getVal( 'wpRatingToken' ) ) ) {
 				// Cross-site request forgery (CSRF) attempt or something, display an error
 				$output .= Html::errorBox( $this->msg( 'sessionfailure' )->parse() );
@@ -133,15 +86,7 @@ class SpecialChangeRating extends SpecialPage {
 			$output .= $this->msg( 'changerating-intro-text', $title->getFullText() )->parseAsBlock()
 				. '<form name="change-rating" action="" method="post">';
 
-			$currentRating = $dbr->selectField(
-				'ratings',
-				'ratings_rating',
-				[
-					'ratings_title' => $title->getDBkey(),
-					'ratings_namespace' => $title->getNamespace()
-				],
-				__METHOD__
-			);
+			$currentRating = self::getCurrentRatingForPage( $title );
 
 			$ratings = RatingData::getAllRatings();
 
@@ -184,6 +129,88 @@ class SpecialChangeRating extends SpecialPage {
 
 			$out->addHTML( $output );
 		}
+	}
+
+	/**
+	 * Given a Title, returns that page's current rating (if any).
+	 *
+	 * @param Title $title
+	 */
+	public static function getCurrentRatingForPage( $title ) {
+		$dbr = wfGetDB( DB_REPLICA );
+		return $dbr->selectField(
+			'ratings',
+			'ratings_rating',
+			[
+				'ratings_title' => $title->getDBkey(),
+				'ratings_namespace' => $title->getNamespace()
+			],
+			__METHOD__
+		);
+	}
+
+	/**
+	 * INSERT a rating for a page if it has none yet, or if it has, UPDATE it to $ratingTo.
+	 *
+	 * @param string $ratingTo New rating (two-character codename)
+	 * @param Title $title The page being rated
+	 */
+	public static function insertOrUpdateRating( $ratingTo, $title ) {
+		$dbw = wfGetDB( DB_PRIMARY );
+		$resOldRating = self::getCurrentRatingForPage( $title );
+		// If there is an entry, update it.
+		// If there isn't, we need to *create* it before we can even
+		// think of updating it :^)
+		// (Whaddya know, trying to UPDATE something that doesn't exist
+		// seems to fail silently, nothing tells you that "you gotta INSERT
+		// first", the code just fails and sends you on a wild goose chase
+		// for half an hour or so...)
+		if ( $resOldRating ) {
+			$dbw->update(
+				'ratings',
+				[ 'ratings_rating' => $ratingTo ],
+				[
+					'ratings_title' => $title->getDBkey(),
+					'ratings_namespace' => $title->getNamespace()
+				],
+				__METHOD__
+			);
+		} else {
+			$dbw->insert(
+				'ratings',
+				[
+					'ratings_rating' => $ratingTo,
+					'ratings_title' => $title->getDBkey(),
+					'ratings_namespace' => $title->getNamespace()
+				],
+				__METHOD__
+			);
+		}
+	}
+
+	/**
+	 * Log a rating change to Special:Log/ratings.
+	 *
+	 * @param User $user The user who performed the action
+	 * @param Title $title Rated page
+	 * @param Rating $rating New Rating object for the given page (Title)
+	 * @param Rating $oldRating Old Rating object for the given page (Title)
+	 * @param string|null $reason User-supplied additional rating comment, if any
+	 */
+	public static function log( $user, $title, $newRating, $oldRating, $reason = null ) {
+		$logEntry = new ManualLogEntry( 'ratings', 'change' );
+		$logEntry->setPerformer( $user );
+		$logEntry->setTarget( $title );
+		$logEntry->setParameters( [
+			'4::newrating' => $newRating->getName(),
+			'5::oldrating' => $oldRating->getName()
+		] );
+		if ( $reason !== null ) {
+			$logEntry->setComment( $reason );
+		}
+
+		$logId = $logEntry->insert();
+		$logEntry->publish( $logId );
 	}
 
 	/**
