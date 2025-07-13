1a79be5754a3dbf24c3caef7fd5aa16ba09595f6
anterdc99 <xuminruixuan@live.cn>
2025-07-13 16:05:14 +0800

    ArticleRatings: Translate special page aliases into Chinese
    Change-Id: I96fb9e4763b0e23d2ca6ac3cd555474686dd0d00

--- ArticleRatings.alias.php
+++ ArticleRatings.alias.php
@@ -21,3 +21,24 @@ $specialPageAliases['fi'] = [
 	'ChangeRating' => [ 'Muuta_arviointia' ],
 	'MassRatings' => [ 'Arvioinnit' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'BatchRating' => [ 'BatchRating' ],
+	'ChangeRating' => [ 'ChangeRating' ],
+	'MassRatings' => [ 'MassRatings' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'BatchRating' => [ '批量评级' ],
+	'ChangeRating' => [ '更改评级' ],
+	'MassRatings' => [ '大量评级' ],
+];
+
+/** Traditional Chinese (中文（繁体）) */
+$specialPageAliases['zh-hant'] = [
+	'BatchRating' => [ '批次評級' ],
+	'ChangeRating' => [ '變更評級' ],
+	'MassRatings' => [ '大量評級' ],
+];
